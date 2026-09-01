1d8a0fedcef9d30ca5c5d977938d25f85040d713
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-01 21:08:41 +0200

    Reading first chapter

--- my_ch01/c2fio.rb
+++ my_ch01/c2fio.rb
@@ -1,7 +1,9 @@
-puts "Reading Celsius temperature value from data file..."
+require "colorize"
+
+puts "Reading Celsius temperature value from data file...".yellow
 celsius = File.read("temp.dat").to_i
 fahrenheit = (celsius * 9 / 5) + 32
-puts "Saving result to output file 'temp.out'"
+puts "Saving result to output file 'temp.out'".blue
 filehandler = File.new("temp.out", "w")
 filehandler.puts fahrenheit
 filehandler.close
