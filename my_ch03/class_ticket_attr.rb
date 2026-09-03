3cc7ffd4593b8508f915738da646f4e9c5cb27d8
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-03 23:20:19 +0200

    Chapter 3 -- attr_accessor

--- my_ch03/class_ticket_attr.rb
+++ my_ch03/class_ticket_attr.rb
@@ -1,6 +1,16 @@
+# class Ticket
+#   attr_reader :venue, :date, :price
+#   attr_writer :price
+
+#   def initialize(venue, date)
+#     @venue = venue
+#     @date = date
+#   end
+# end
+
 class Ticket
-  attr_reader :venue, :date, :price
-  attr_writer :price
+  attr_reader :venue, :date
+  attr_accessor :price
 
   def initialize(venue, date)
     @venue = venue
