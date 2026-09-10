a919abbe219e54a331dcbfec612333308d6532ec
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-10 23:30:52 +0200

    Add more examples of self

--- ch05/5.2.rb
+++ ch05/5.2.rb
@@ -1,14 +1,18 @@
-class Person 
-  attr_accessor :first_name, :middle_name, :last_name 
-  def whole_name 
-    n = first_name + " " 
-    n << "#{middle_name} " if middle_name 
+# frozen_string_literal: true
+
+# method calls on implicit self
+class Person
+  attr_accessor :first_name, :middle_name, :last_name
+
+  def whole_name
+    n = "#{first_name} "
+    n << "#{middle_name} " if middle_name
     n << last_name
-  end 
-end 
-david = Person.new 
-david.first_name = "David" 
-david.last_name = "Black" 
-puts "David's whole name: #{david.whole_name}" 
-david.middle_name = "Alan" 
-puts "David's new whole name: #{david.whole_name}" 
+  end
+end
+david = Person.new
+david.first_name = 'David'
+david.last_name = 'Black'
+puts "David's whole name: #{david.whole_name}"
+david.middle_name = 'Alan'
+puts "David's new whole name: #{david.whole_name}"
