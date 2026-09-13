c3951908f7e4814e121f8d1b17f2d7de7950fb00
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-13 09:07:32 +0200

    Example of class variables

--- ch05/5.6.rb
+++ ch05/5.6.rb
@@ -1,28 +1,35 @@
+# frozen_string_literal: true
+
+#  This is a class for a car.
 class Car
-  @@makes = []
-  @@cars = {}
-  @@total_count = 0
+  @@makes = [] # rubocop:disable Style/ClassVars
+  @@cars = {} # rubocop:disable Style/ClassVars
+  @@total_count = 0 # rubocop:disable Style/ClassVars
   attr_reader :make
+
   def self.total_count
     @@total_count
   end
+
   def self.add_make(make)
-    unless @@makes.include?(make)
+    unless @@makes.include?(make) # rubocop:disable Style/GuardClause
       @@makes << make
       @@cars[make] = 0
     end
   end
+
   def initialize(make)
-    if @@makes.include?(make)
+    if @@makes.include?(make) # rubocop:disable Style/GuardClause
       puts "Creating a new #{make}!"
       @make = make
       @@cars[make] += 1
-      @@total_count += 1
+      @@total_count += 1 # rubocop:disable Style/ClassVars
     else
       raise "No such make: #{make}."
     end
   end
+
   def make_mates
-    @@cars[self.make]
+    @@cars[self.make] # rubocop:disable Style/RedundantSelf
   end
 end
