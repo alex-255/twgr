d07e224e6cf69d45b533eddaa49d8ff09e4edcda
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-15 20:57:21 +0200

    Fix Rubocop's suggestions

--- ch06/6.3.rb
+++ ch06/6.3.rb
@@ -1,10 +1,12 @@
-class Temperature
-  def Temperature.c2f(celsius)
+# frozen_string_literal: true
+
+class Temperature # rubocop:disable Style/Documentation
+  def self.c2f(celsius)
     celsius * 9.0 / 5 + 32
   end
 end
 celsius = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 puts "Celsius\tFahrenheit"
-for c in celsius
+for c in celsius # rubocop:disable Style/For
   puts "#{c}\t#{Temperature.c2f(c)}"
 end
