a354266e470490da3fdd7a25937443c54e26d08d
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-15 22:03:42 +0200

    Try to solve exercise my_times method

---
+++ ch06/my_each__to__my_times.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# nice try
+
+# class Array
+#   def my_each
+#     c = 0
+#     until c == size
+#       yield(self[c])
+#       c += 1
+#     end
+#     self
+#   end
+# end
+
+# class Integer
+#   def self.my_times(&block)
+#     Array.my_each(&block)
+#   end
+# end
