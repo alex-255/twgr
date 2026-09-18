4cde0e9afe28006d7a367c26ef578e4e716d27e3
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-18 21:20:04 +0200

    The class Account for bank accounts

--- ch07/account.rb
+++ ch07/account.rb
@@ -1,14 +1,21 @@
+# frozen_string_literal: true
+
+# A bank account class
 class Account
   attr_accessor :balance
-  def initialize(amount=0)
+
+  def initialize(amount = 0)
     self.balance = amount
   end
-  def +(x)
+
+  def +(x) # rubocop:disable Naming/MethodParameterName,Naming/BinaryOperatorParameterName
     self.balance += x
   end
-  def -(x)
+
+  def -(x) # rubocop:disable Naming/MethodParameterName,Naming/BinaryOperatorParameterName
     self.balance -= x
   end
+
   def to_s
     balance.to_s
   end
@@ -16,4 +23,3 @@ end
 acc = Account.new(20)
 acc -= 5
 puts acc
-
